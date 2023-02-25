ac0056f99b6e94c1a5f1810a0a9fe600eb8e7b56
ACTION-FIRE <122881764+ACTION-FIRE@users.noreply.github.com>
2023-02-25 17:25:57 +0300

    Delete Random.py

--- Random.py
+++
@@ -1,6 +0,0 @@
-
-#Compiled By ACTION-FIRE
-#github : https://github.com/ACTION-FIRE
-#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>
-import marshal
-exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\>
