d13e0f976d35be69a0b7bedd945deb6c70625714
ACTION-FIRE <122881764+ACTION-FIRE@users.noreply.github.com>
2023-02-25 17:22:55 +0300

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,6 @@
+
+#Compiled By ACTION-FIRE
+#github : https://github.com/ACTION-FIRE
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>
+import marshal
+exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\>
